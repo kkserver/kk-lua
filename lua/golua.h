810d97a661c399c450c489502d08e14e3104acda
zhang hailong <hailongz@qq.com>
2017-09-22 17:46:33 +0800

    Update

--- lua/golua.h
+++ lua/golua.h
@@ -1,7 +1,12 @@
 #include <stdint.h>
 
+#ifndef KK_GO_LUA_H
+#define KK_GO_LUA_H
+
 typedef void * kk_uintptr;
 
 kk_uintptr clua_newobject(struct lua_State * L);
 
 kk_uintptr clua_newfunction(struct lua_State *L);
+
+#endif
